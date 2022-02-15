4e9ce20769ddec1a52426d1fb515eb24b5ad4fb8
ztmaev <97959215+ztmaev@users.noreply.github.com>
2022-02-15 17:00:32 +0300

    Add files via upload

---
+++ simple calculator/main.c
@@ -0,0 +1,40 @@
+/*
+simple calculator
+By maev
+last updated 15th FEB 2022
+C89 compiiler
+MIT license
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+// variable declaration
+
+char name[100];
+int n1,n2,sum,product,diff;
+float quot;
+    printf("Simple Calculator!\n");
+
+    //input
+    printf("Enter your name:");
+    gets(name);
+    printf("Enter two integers:");
+    scanf("%d%d",&n1,&n2);
+
+    //computations
+    sum = n1 +n2;
+    diff = n1 - n2;
+    product = n1 * n2;
+    quot = (float) n1/n2;
+
+    //output
+    printf("Hey %s, here are the results: \n" ,name);
+    printf("%d+d%d=%d\n",n1,n2,sum);
+    printf("%d-d%d=%d\n",n1,n2,diff);
+    printf("%d*d%d=%d\n",n1,n2,product);
+    printf("%d/d%d=%f\n",n1,n2,quot);
+    return 0;
+
+}
